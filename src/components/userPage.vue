c6ebfbfebb7e209cd492cda274ae63e17721f60a
Zlei <48239392+16206463@users.noreply.github.com>
2020-04-02 23:44:16 +0800

    相机能动了

--- src/components/userPage.vue
+++ src/components/userPage.vue
@@ -5,7 +5,18 @@
     <h3>{{ message }}</h3>
 <!--    <button @click='ky' >c跨域csdn</button>-->
     <button @click='deleteuser' >删除用户</button>
+
+
+    <div class="camera_outer">
+      <video id="videoCamera" :width="videoWidth" :height="videoHeight" autoplay></video>
+      <canvas style="display:none;" id="canvasCamera" :width="videoWidth" :height="videoHeight" ></canvas>
+      <button @click="getCompetence"> 打开相机 </button>
+      <button @click="pick"> 拍照 </button>
+      <button @click="stopNavigator"> 停 </button>
+
+    </div>
 	</div>
+  
 
 </template>
 
@@ -17,21 +28,16 @@
     data(){
       return{
         message: '',
+        videoWidth: 540,
+        videoHeight: 410,
+        imgSrc: '',
+        thisVideo: null,
       }
     },
-		methods: {
 
-      // ky() {
-      //   console.log('click')
-      //
-      //   axios({
-      //     url: '/api/articles?type=more&category=home&shown_offset=1524276761019196&first_view=false',
-      //   }).then(res => {
-      //     console.log(res);
-      //     this.message = res
-      //   })
-      //
-      // },
+    computed: {
+    },
+    methods: {
 
       deleteuser() {
         console.log('click')
@@ -44,12 +50,60 @@
           this.message = res
         })
 
-      }
+      },
 
+      getCompetence () {
+        var _this = this
+        this.thisVideo = document.getElementById('videoCamera')
+
+        var constraints = { audio: false, video: { width: this.videoWidth, height: this.videoHeight } }
+
+        navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+          _this.thisVideo.srcObject = stream
+          _this.thisVideo.onloadedmetadata = function (e) {
+            _this.thisVideo.play()
+          }
+        }).catch(err => {
+          console.log(err)
+        })
+      },
+
+      stopNavigator () {
+        this.thisVideo.srcObject.getTracks()[0].stop()
+      },
+
+      pick(){
+
+      }
+    },
+    mounted () {
+      this.getCompetence()
+    },
+    beforeDestroy () {
+      this.stopNavigator()
     }
+
 	}
 </script>
 
 <style type="text/css">
+  .camera_outer {
+    position: relative;
+    overflow: hidden;
+    background-size: 100%;
+
+  }
+  video,canvas,.tx_img{
+    -moz-transform:scaleX(-1);
+    -webkit-transform:scaleX(-1);
+    -o-transform:scaleX(-1);
+    transform:scaleX(-1);
+  }
+
+  img{
+    width: 100%;
+    height: 100%;
+  }
+
 
 </style>
