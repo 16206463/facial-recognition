cda313f3bce3e2e7103f05e0aadfa19275d43bfc
Zlei <48239392+16206463@users.noreply.github.com>
2020-04-07 16:01:14 +0800

    Revert "Merge branch 'master' of https://github.com/16206463/facialrecognition"
    This reverts commit 038d9a7cd26bf2beeba919cd1c6565995510ac85, reversing
    changes made to d9ad0e0acb9ae42838fecf41f06a11435fc3f8d7.

--- src/components/loginPage.vue
+++ src/components/loginPage.vue
@@ -1,46 +1,23 @@
 <template>
   <div class="login-info">
-    <!--<el-row>-->
-      <!--<el-col >-->
-        <!--<h3 style="text-align: center;"> login page </h3>-->
-        <!--<el-form :model="ruleForm"  :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">-->
-          <!--<el-form-item label="账号：" prop="account">-->
-            <!--<el-input  v-model="ruleForm.account" autocomplete="off" clearable>-->
-            <!--</el-input>-->
-          <!--</el-form-item>-->
-          <!--<el-form-item label="密码：" prop="password">-->
-            <!--<el-input type="password" v-model="ruleForm.password" autocomplete="off" show-password clearable></el-input>-->
-          <!--</el-form-item>-->
-          <!--<el-form-item>-->
-            <!--<el-button type="primary" @click="submitusrForm('ruleForm')">添加新用户并登陆</el-button>-->
-            <!--<el-button type="primary" @click="submitadForm('ruleForm')">添加新管理员并登陆</el-button>-->
-          <!--</el-form-item>-->
-        <!--</el-form>-->
-      <!--</el-col>-->
-    <!--</el-row>-->
-    <div class="container">
-      <transition name="form-fade" mode="in-out">
-        <section class="form_contianer" v-show="showLogin">
-          <div class="manage_tip">
-            <p>Welcome</p>
-          </div>
-          <el-form :model="loginForm" :rules="rules" ref="loginForm">
-            <el-form-item prop="username">
-              <el-input v-model="loginForm.username" placeholder="User Name"><span>dsfsf</span></el-input>
-            </el-form-item>
-            <el-form-item prop="password">
-              <el-input type="password" placeholder="Password" v-model="loginForm.password"></el-input>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">Log in</el-button>
-            </el-form-item>
-          </el-form>
-          <p class="tip">Automatic registration of new users.</p>
-          <p class="tip">Registered users log in with user name and password.</p>
-        </section>
-      </transition>
-    </div>
-
+    <el-row>
+      <el-col >
+        <h3 style="text-align: center;"> login page </h3>
+        <el-form :model="ruleForm"  :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+          <el-form-item label="账号：" prop="account">
+            <el-input  v-model="ruleForm.account" autocomplete="off" clearable>
+            </el-input>
+          </el-form-item>
+          <el-form-item label="密码：" prop="password">
+            <el-input type="password" v-model="ruleForm.password" autocomplete="off" show-password clearable></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitusrForm('ruleForm')">添加新用户并登陆</el-button>
+            <el-button type="primary" @click="submitadForm('ruleForm')">添加新管理员并登陆</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -65,33 +42,20 @@
         }
       };
       return {
-        // ruleForm: {
-        //   account: '',
-        //   password: '',
-        // },
-        // rules: {
-        //   account: [{
-        //     validator: validateAccount,
-        //     trigger: 'blur'
-        //   }],
-        //   password: [{
-        //     validator: validatePassword,
-        //     trigger: 'blur'
-        //   }]
-        // },
-        loginForm: {
-          username: '',
+        ruleForm: {
+          account: '',
           password: '',
         },
         rules: {
-          username: [
-            { required: true, message: 'Please enter user name', trigger: 'blur' },
-          ],
-          password: [
-            { required: true, message: 'Please enter password', trigger: 'blur' }
-          ],
-        },
-        showLogin: false,
+          account: [{
+            validator: validateAccount,
+            trigger: 'blur'
+          }],
+          password: [{
+            validator: validatePassword,
+            trigger: 'blur'
+          }]
+        }
       };
     },
     methods: {
@@ -170,36 +134,8 @@
 </script>
 
 <style scoped>
-  .login-info{
+  .login-info >>> .el-col{
     /*background-color: #e5e8ec;*/
-    /*padding: 2% 5% 0% 2%;*/
-    width:300px;
-    height:320px;
+    padding: 2% 5% 0% 2%;
   }
-  .manage_tip{
-    position: absolute;
-    width: 100%;
-    top: -100px;
-    left: 0;
-  }
-  .container {
-
-    height:100%;width:100%;
-    /*width: 100%;*/
-    border: 1px solid green;
-    position:fixed;
-
-  }
-  .form_contianer{
-
-    padding: 25px;
-    border-radius: 5px;
-    text-align: center;
-    background-color: #fff;
-  }
-  .tip{
-    font-size: 12px;
-    color: #023845;
-  }
-
 </style>
