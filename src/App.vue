84525528957c9ca17d9568b11f8f4b5f3c127a81
Zlei <48239392+16206463@users.noreply.github.com>
2020-04-08 14:52:17 +0800

    恢复前端

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,37 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/dogpic.jpg"  width="300" height="280"> -->
-     
+    <!-- < img src="./assets/dogpic.jpg"  width="300" height="280"> -->
+
     <router-link to="/loginPage" tag="button"> 登陆页 </router-link>
     <router-link to="/facialRecognition" tag="button" > 识别页 </router-link>
     <router-link :to="'/userPage/'+userId" tag="button" > 用户页 </router-link>
-    
+
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      userId: 'zl',
+  export default {
+    name: 'App',
+    data() {
+      return {
+        userId: 'zl',
+      }
     }
   }
-}
 </script>
 
-<style lang="less">
-  @import './style/common';
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 
-.router-link-active{
-  color: red;
-}
+  .router-link-active{
+    color: red;
+  }
 </style>
--- src/components/loginPage.vue
+++ src/components/loginPage.vue
@@ -1,23 +1,24 @@
 <template>
+  <div class="login-info">
     <!--<el-row>-->
-      <!--<el-col >-->
-        <!--<h3 style="text-align: center;"> login page </h3>-->
-        <!--<el-form :model="ruleForm"  :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">-->
-          <!--<el-form-item label="账号：" prop="account">-->
-            <!--<el-input  v-model="ruleForm.account" autocomplete="off" clearable>-->
-            <!--</el-input>-->
-          <!--</el-form-item>-->
-          <!--<el-form-item label="密码：" prop="password">-->
-            <!--<el-input type="password" v-model="ruleForm.password" autocomplete="off" show-password clearable></el-input>-->
-          <!--</el-form-item>-->
-          <!--<el-form-item>-->
-            <!--<el-button type="primary" @click="submitusrForm('ruleForm')">添加新用户并登陆</el-button>-->
-            <!--<el-button type="primary" @click="submitadForm('ruleForm')">添加新管理员并登陆</el-button>-->
-          <!--</el-form-item>-->
-        <!--</el-form>-->
-      <!--</el-col>-->
+    <!--<el-col >-->
+    <!--<h3 style="text-align: center;"> login page </h3>-->
+    <!--<el-form :model="ruleForm"  :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">-->
+    <!--<el-form-item label="账号：" prop="account">-->
+    <!--<el-input  v-model="ruleForm.account" autocomplete="off" clearable>-->
+    <!--</el-input>-->
+    <!--</el-form-item>-->
+    <!--<el-form-item label="密码：" prop="password">-->
+    <!--<el-input type="password" v-model="ruleForm.password" autocomplete="off" show-password clearable></el-input>-->
+    <!--</el-form-item>-->
+    <!--<el-form-item>-->
+    <!--<el-button type="primary" @click="submitusrForm('ruleForm')">添加新用户并登陆</el-button>-->
+    <!--<el-button type="primary" @click="submitadForm('ruleForm')">添加新管理员并登陆</el-button>-->
+    <!--</el-form-item>-->
+    <!--</el-form>-->
+    <!--</el-col>-->
     <!--</el-row>-->
-    <div class="login_page fillcontain">
+    <div class="container">
       <transition name="form-fade" mode="in-out">
         <section class="form_contianer" v-show="showLogin">
           <div class="manage_tip">
@@ -31,10 +32,7 @@
               <el-input type="password" placeholder="Password" v-model="loginForm.password"></el-input>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')" class="submit_btn">Log in</el-button>
-            </el-form-item>
-            <el-form-item>
-              <el-button type="primary" @click="submitForm('ruleForm')" class="submit_btn">Admin Login</el-button>
+              <el-button type="primary" @click="submitForm('loginForm')" class="submit_btn">Log in</el-button>
             </el-form-item>
           </el-form>
           <p class="tip">Automatic registration of new users.</p>
@@ -43,6 +41,7 @@
       </transition>
     </div>
 
+  </div>
 </template>
 
 <script>
@@ -66,20 +65,33 @@
         }
       };
       return {
-        ruleForm: {
-          account: '',
+        // ruleForm: {
+        //   account: '',
+        //   password: '',
+        // },
+        // rules: {
+        //   account: [{
+        //     validator: validateAccount,
+        //     trigger: 'blur'
+        //   }],
+        //   password: [{
+        //     validator: validatePassword,
+        //     trigger: 'blur'
+        //   }]
+        // },
+        loginForm: {
+          username: '',
           password: '',
         },
         rules: {
-          account: [{
-            validator: validateAccount,
-            trigger: 'blur'
-          }],
-          password: [{
-            validator: validatePassword,
-            trigger: 'blur'
-          }]
-        }
+          username: [
+            { required: true, message: 'Please enter user name', trigger: 'blur' },
+          ],
+          password: [
+            { required: true, message: 'Please enter password', trigger: 'blur' }
+          ],
+        },
+        showLogin: false,
       };
     },
     methods: {
@@ -91,6 +103,7 @@
             params.append('admin', 0); //你要传给后台的参数值 key/value
             params.append('username', this.ruleForm.account);
             params.append('password', this.ruleForm.password);
+
             axios({
               url: '/dashboard/add_user',
               data: params,
@@ -156,44 +169,37 @@
   }
 </script>
 
-<style lang="less" scoped>
-  @import '../style/mixin';
-  .login_page{
-    width: 100%;
-    height: 100%;
-    background: #c5f1fc;
+<style scoped>
+  .login-info{
+    /*background-color: #e5e8ec;*/
+    /*padding: 2% 5% 0% 2%;*/
+    width:300px;
+    height:320px;
   }
   .manage_tip{
     position: absolute;
     width: 100%;
     top: -100px;
     left: 0;
-    p{
-      font-size: 50px;
-      color: #023845;
-    }
+  }
+  .container {
+
+    height:100%;width:100%;
+    /*width: 100%;*/
+    border: 1px solid green;
+    position:fixed;
+
   }
   .form_contianer{
-    .wh(320px, 210px);
-    .ctp(320px, 210px);
+
     padding: 25px;
     border-radius: 5px;
     text-align: center;
     background-color: #fff;
-    .submit_btn{
-      width: 100%;
-      font-size: 16px;
-    }
   }
   .tip{
     font-size: 12px;
     color: #023845;
   }
-  .form-fade-enter-active, .form-fade-leave-active {
-    transition: all 1s;
-  }
-  .form-fade-enter, .form-fade-leave-active {
-    transform: translate3d(0, -50px, 0);
-    opacity: 0;
-  }
+
 </style>
